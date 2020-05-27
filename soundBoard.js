2838c975d6a98d5974cd8d38cbfb426e7e7acdaf
Blairwin <reach.blair@gmail.com>
2020-05-26 21:18:06 -0400

    the one with the CORS error haha

--- soundBoard.js
+++ soundBoard.js
@@ -1,3 +1,25 @@
+getSounds();
+
+const soundElement = document.querySelector('#sounds');
+
+(async () => {
+	const sounds = await getSounds();
+	addSoundsToPage(sounds);
+});
+
+
 async function getSounds() {
-    const response = await fetch('./sounds.json')
+    const response = await fetch("./sounds.json");
+    const json = await response.json();
+    return json;
+}
+
+function addSoundsToPage(sounds) {
+    sounds.forEach(sound => {
+        const soundDiv = document.createElement('div');
+        const soundTitle = document.createElement('h2');
+        soundTitle.textContent = sound.title;
+        soundDiv.appendChild(soundTitle);
+        soundsElement.appendChild(soundDiv);
+    })
 }
