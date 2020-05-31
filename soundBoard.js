d4a69dcc4b9c2380e480859db595aca22d579ac1
Blairwin <reach.blair@gmail.com>
2020-05-31 00:22:19 -0400

    added random song button and lots of javascript

--- soundBoard.js
+++ soundBoard.js
@@ -1,4 +1,4 @@
-// Note to self: simplify this! forEach?
+// Note to self: simplify this! forEach? function. 
 const player = document.getElementById("audio");
 const player2 = document.getElementById("audio2");
 const player3 = document.getElementById("audio3");
@@ -9,101 +9,160 @@ const player7 = document.getElementById("audio7");
 const player8 = document.getElementById("audio8");
 const player9 = document.getElementById("audio9");
 
-// push button & song plays
+// // click image & song plays or pauses
+const button = document.getElementById("img");
 document.getElementById("img").addEventListener("click", () => {
-    document.getElementById("audio").play();
-    // press it again and it restarts
-    player.currentTime = 0;
-    player.play();
+    if (player.paused) {
+		player.play();
+	} else {
+		player.pause();
+	}
 });
 
+const button2 = document.getElementById("img2");
 document.getElementById("img2").addEventListener("click", () => {
-    document.getElementById("audio2").play();
-    player2.currentTime = 0;
-	player2.play();
+	if (player2.paused) {
+		player2.play();
+	} else {
+		player2.pause();
+	}
 });
 
+const button3 = document.getElementById("img3");
 document.getElementById("img3").addEventListener("click", () => {
-    document.getElementById("audio3").play();
-    player3.currentTime = 0;
-	player3.play();
+	if (player3.paused) {
+		player3.play();
+	} else {
+		player3.pause();
+	}
 });
 
+const button4 = document.getElementById("img4");
 document.getElementById("img4").addEventListener("click", () => {
-    document.getElementById("audio4").play();
-    player4.currentTime = 0;
-	player4.play();
+	if (player4.paused) {
+		player4.play();
+	} else {
+		player4.pause();
+	}
 });
 
+const button5 = document.getElementById("img5");
 document.getElementById("img5").addEventListener("click", () => {
-    document.getElementById("audio5").play();
-    player5.currentTime = 0;
-	player5.play();
+	if (player5.paused) {
+		player5.play();
+	} else {
+		player5.pause();
+	}
 });
 
+const button6 = document.getElementById("img6");
 document.getElementById("img6").addEventListener("click", () => {
-    document.getElementById("audio6").play();
-    player6.currentTime = 0;
-	player6.play();
+	if (player6.paused) {
+		player6.play();
+	} else {
+		player6.pause();
+	}
 });
 
+const button7 = document.getElementById("img7");
 document.getElementById("img7").addEventListener("click", () => {
-    document.getElementById("audio7").play();
-    player7.currentTime = 0;
-	player7.play();
+	if (player7.paused) {
+		player7.play();
+	} else {
+		player7.pause();
+	}
 });
 
+const button8 = document.getElementById("img8");
 document.getElementById("img8").addEventListener("click", () => {
-    document.getElementById("audio8").play();
-    player8.currentTime = 0;
-	player8.play();
+	if (player8.paused) {
+		player8.play();
+	} else {
+		player8.pause();
+	}
 });
 
+const button9 = document.getElementById("img9");
 document.getElementById("img9").addEventListener("click", () => {
-    document.getElementById("audio9").play();
-    player9.currentTime = 0;
-	player9.play();
+	if (player9.paused) {
+		player9.play();
+	} else {
+		player9.pause();
+	}
+});
+
+// changes play icon to pause on click and back again
+// HOW TO APPLY IT to ALL of the images and not just one 
+document.querySelector('.image').addEventListener("click", function () {
+	const icon = this.querySelector("i");
+	if (icon.classList.contains("fa-play")) {
+		icon.classList.remove("fa-play");
+		icon.classList.add("fa-pause");
+	} else {
+		icon.classList.remove("fa-pause");
+		icon.classList.add("fa-play");
+	}
 });
 
 
+// click button to get a random song 
 
-// getSounds();
+function getSong() {
+	const song = [
+		{
+			title: "Times Like This",
+			src: "./audioFiles/timesLikeThisSoundBoard.mp3",
+		},
+		{
+			title: "I Won't Mind",
+			src: "./audioFiles/iWontMindSoundBoard.mp3",
+		},
+		{
+			title: "A Day With Julia",
+			src: "./audioFiles/aDayWithJuliaSoundBoard.mp3",
+		},
+		{
+			title: "Lion Tamer",
+			src: "./audioFiles/lionTamerSoundBoard.mp3",
+		},
+		{
+			title: "Over The Rainbow",
+			src: "./audioFiles/overTheRainbowSoundBoard.mp3",
+		},
+		{
+			title: "I Dreamed A Dream",
+			src: "./audioFiles/iDreamedADreamSoundBoard.mp3",
+		},
+		{
+			title: "Another Hundred People",
+			src: "./audioFiles/anotherHundredPeopleSoundBoard.mp3",
+		},
+		{
+			title: "A Way Back To Then",
+			src: "./audioFiles/aWayBackToThenSoundBoard.mp3",
+		},
+		{
+			title: "Defying Gravity",
+			src: "./audioFiles/defyingGravitySoundBoard.mp3",
+		},
+	];
 
-// const soundsElement = document.querySelector('#sounds');
+    const randomSong = song[Math.floor(Math.random() * song.length)];
 
-// (async () => {
-// 	const sounds = await getSounds();
-// 	addSoundsToPage(sounds);
-// })();
+    // ADD stopping one song before another starts on random 
+    const audio = new Audio(randomSong.src);
+	audio.play();
+}
 
+document.querySelector('button').addEventListener("click", () => {
+    getSong();
+});
 
-// async function getSounds() {
-//     const response = await fetch("./sounds.json");
-//     const json = await response.json();
-//     return json;
-// }
-
-// function addSoundsToPage(sounds) {
-//     sounds.forEach(sound => {
-//         const soundDiv = document.createElement('div');
-//         const soundTitle = document.createElement('h2');
-//         soundTitle.textContent = sound.title;
-//         soundDiv.appendChild(soundTitle);
-
-//         const play = document.createElement('button');
-//         play.textContent = 'Play';
-//         soundDiv.appendChild(play); 
-
-        // when button is clicked play song
-        // play.addEventListener('click', () => {
-            // allow user to start it again before it's done 
-        //     player.currentTime = 0;
-        //     player.play();
-        // });
-
-//         const player = document.createElement('audio');
-//         player.setAttribute('src', `audioFiles/${sound.src}`)
-//         soundDiv.appendChild(player);
-//         soundsElement.appendChild(soundDiv);
-//     })
-// }
+// pause all music 
+// it would be great to figure out a better solution to stopping the audio for the random button 
+document.querySelector('.quiet').onclick = function () {
+	// const sounds = document.getElementsByTagName("audio");
+    // for (i = 0; i < sounds.length; i++) sounds[i].pause();
+    location.reload();
+	return false;
+};
